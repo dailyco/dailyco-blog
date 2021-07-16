bf2ca9568d2b57268882970310120ad906c25a2d
dailyco <dailyco03@gmail.com>
2021-07-16 11:33:51 +0900

    styling: post divier 생성

--- src/components/thumbnail-item/index.jsx
+++ src/components/thumbnail-item/index.jsx
@@ -5,10 +5,13 @@ import { TARGET_CLASS } from '../../utils/visible'
 import './index.scss'
 
 export const ThumbnailItem = ({ node }) => (
-  <Link className={`thumbnail ${TARGET_CLASS}`} to={node.fields.slug}>
-    <div key={node.fields.slug}>
-      <h3>{node.frontmatter.title || node.fields.slug}</h3>
-      <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
-    </div>
-  </Link>
+  <>
+    <Link className={`thumbnail ${TARGET_CLASS}`} to={node.fields.slug}>
+      <div key={node.fields.slug}>
+        <h3>{node.frontmatter.title || node.fields.slug}</h3>
+        <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+      </div>
+    </Link>
+    <div className={'divider'}></div>
+  </>
 )
